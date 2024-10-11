f8038ea6e9119a55b73d8ce1c0176145988a92c9
abhs27 <118160173+abhs27@users.noreply.github.com>
2024-10-11 18:51:56 +0530

    Create adder.v

---
+++ Adder(PC)/adder.v
@@ -0,0 +1,10 @@
+//Primarily used for PC incrementing and branching
+module Adder (
+    input signed [31:0] a,
+    input signed [31:0] b,
+    output signed [31:0] sum
+);
+
+assign sum = a + b;
+
+endmodule  
