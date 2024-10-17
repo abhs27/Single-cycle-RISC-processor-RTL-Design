067bbf2e4dd35e77e52baddab0a7a8e7d0e3fc30
abhs27 <118160173+abhs27@users.noreply.github.com>
2024-10-17 22:22:49 +0530

    Create ALUCntrl.v

---
+++ ALU Control Unit/ALUCntrl.v
@@ -0,0 +1,19 @@
+module ALUCtrl (
+    input [1:0] ALUOp,
+    input funct7,
+    input [2:0] funct3,
+    output reg [3:0] ALUCtl
+);
+
+    // TODO: implement your ALU ALUCtl here
+   // Hint: using ALUOp, funct7, funct3 to select exact operation
+always@(*)
+begin
+	case(ALUOp):begin
+		2b'
+end
+
+
+
+
+endmodule
