62c9e3dffbe453630f9073514253af3a2c438106
abhs27 <118160173+abhs27@users.noreply.github.com>
2024-10-17 23:06:39 +0530

    fixed syntax errors

--- ALU Control Unit/ALUCntrl.v
+++ ALU Control Unit/ALUCntrl.v
@@ -10,30 +10,30 @@ module ALUCtrl (
 always@(*)
 begin
 	case(ALUOp):begin //checking whether ld/str or B-type or R-type
-		2b'00 : ALUCntrl = 4b'0010; //load or store word
-		2b'01 : ALUCntrl = 4b'0110; //branch
-		2b'10 :
+		2'b00 : ALUCntrl = 4b'0010; //load or store word
+		2'b01 : ALUCntrl = 4b'0110; //branch
+		2'b10 :
 		begin
 		case(funct3): //checking which R-type instruction
-		3b'000 :
+		3'b000 :
 			if(funct7)
-				ALUCntrl = 4b'0110; //subtract
+				ALUCntrl = 4'b0110; //subtract
 			else
-				ALUCntrl = 4b'0010; //add
-		3b'001 : ALUCntrl = 4b'1010; //Shift left logical
-		3b'010 : ALUCntrl = 4b'0100; //set less than signed
-		3b'011 : ALUCntrl = 4b'0101; //set less than unsigned
-		3b'100 : ALUCntrl = 4b'0011; //XOR
-		3b'101 : 
+				ALUCntrl = 4'b0010; //add
+		3'b001 : ALUCntrl = 4'b1010; //Shift left logical
+		3'b010 : ALUCntrl = 4'b0100; //set less than signed
+		3'b011 : ALUCntrl = 4'b0101; //set less than unsigned
+		3'b100 : ALUCntrl = 4'b0011; //XOR
+		3'b101 : 
 			if(funct7) 
-				ALUCntrl = 4b'1001; //shift right arithmetic
+				ALUCntrl = 4'b1001; //shift right arithmetic
 			else
-				ALUCntrl = 4b'1000; //shift right logical
-		3b'110 : ALUCntrl = 4b'0001; //OR
-		3b'111 : ALUCntrl = 4b'0000; //AND
+				ALUCntrl = 4'b1000; //shift right logical
+		3'b110 : ALUCntrl = 4'b0001; //OR
+		3'b111 : ALUCntrl = 4'b0000; //AND
 		end
 
-		 ALUCntrl = 4b'default : ALUCntrl = 4b'1111; //not supposed to happen
+		default : ALUCntrl = 4'b1111; //not supposed to happen
 end
 
 
